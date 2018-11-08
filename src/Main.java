2698173996e5f142beb524ebcca2edc71e26e868
Iaroslav Pavlynskyi <iaroslav.pavlynskyi@globallogic.com>
2018-11-08 14:34:23 +0200

    Added one more line

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hi, again");
+        System.out.println("Create patch");
+        System.out.println("One more line");
     }
 }
